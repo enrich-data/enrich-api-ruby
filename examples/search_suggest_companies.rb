2904e8b2eff36f43f63eefad832dc77537e3bfb3
Valerian Saliou <valerian@valeriansaliou.name>
2017-09-09 14:39:03 +0200

    Fix max line len
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- examples/search_suggest_companies.rb
+++ examples/search_suggest_companies.rb
@@ -9,7 +9,10 @@ require 'graphmob'
 
 client = Graphmob::Client.new
 
-client.authenticate("ui_a311da78-6b89-459c-8028-b331efab20d5", "sk_f293d44f-675d-4cb1-9c78-52b8a9af0df2")
+client.authenticate(
+  "ui_a311da78-6b89-459c-8028-b331efab20d5",
+  "sk_f293d44f-675d-4cb1-9c78-52b8a9af0df2"
+)
 
 data = client.search.suggest_companies({
   company_name: "Crisp"
